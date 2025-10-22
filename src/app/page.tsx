35988bda710dc6e851a6886f3e65f202b2225934
uzakotim <timauzakov@gmail.com>
2025-10-22 07:59:18 +0500

    2. Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
   return (
-   <div>Hello world
-    <Button>Click me</Button>
+   <div>{JSON.stringify(users)}
    </div> 
   );
 }
